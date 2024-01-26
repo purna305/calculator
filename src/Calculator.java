345b200aa3b6dc3cbb73bc6ac433ec51680b1311
purna305 <97538633+purna305@users.noreply.github.com>
2024-01-26 18:47:41 +0530

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,5 +2,6 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("calculator class is created");
         System.out.println("Subraction function is added");
+        System.out.println("Addition feature is added");
     }
 }
